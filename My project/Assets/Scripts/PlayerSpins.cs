9e383ed38240d72a09367bfa8a8b1cd445e738c8
Piotr Łach <lachpio999@gmail.com>
2022-02-21 16:56:19 +0100

    player spins script 1.0
    nawet nie jest fajny

---
+++ My project/Assets/Scripts/PlayerSpins.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpins : MonoBehaviour
+{
+    public float xAngle = 0, yAngle = 0, zAngle = 0, zRotate = 1;
+
+    private GameObject playerCube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCube = GameObject.Find("Player");
+    }
+
+    bool CheckIsPlayerSpin()
+    {
+        return false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerCube.transform.Rotate(xAngle, yAngle, +zRotate, Space.Self);
+    }
+}
