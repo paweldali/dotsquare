0e64db243db4689ca50047fafa94abd55c29c46c
paweldali <pawel@dali.games>
2022-02-24 18:17:40 +0100

    save

--- My project/Assets/Scripts/Timer.cs
+++ My project/Assets/Scripts/Timer.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
     public float currentTime = 0f;
 
-    [SerializeField] Text timerText;
+    [SerializeField] TextMeshProUGUI timerText;
 
     // Start is called before the first frame update
     void Start()
