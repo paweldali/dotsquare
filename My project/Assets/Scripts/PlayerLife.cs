c1c3d214bd95b599df29bba6c03ff20c390a53fe
paweldali <pawel@dali.games>
2022-02-26 17:06:12 +0100

    player death animation done

--- My project/Assets/Scripts/PlayerLife.cs
+++ My project/Assets/Scripts/PlayerLife.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerLife : MonoBehaviour
 {
@@ -27,5 +28,10 @@ public class PlayerLife : MonoBehaviour
     {
        anim.SetTrigger("Death"); 
        rb.bodyType = RigidbodyType2D.Static;
+    }
+
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
