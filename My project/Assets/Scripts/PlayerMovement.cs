40ebfbb20634bd0c7e87d4176b61282fa8f062b9
Paweł Łach <pawel@dali.games>
2022-02-28 22:06:28 +0100

    save

--- My project/Assets/Scripts/PlayerMovement.cs
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -2,11 +2,10 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-public float MovementSpeed = 1;
-public float JumpForce = 1;
-
-private Rigidbody2D _rigidbody;
-
+    public float MovementSpeed = 1;
+    public float JumpForce = 1;
+    public float SpeedMultiplier = 2;
+    private Rigidbody2D _rigidbody;
     // Start is called before the first frame update
     private void Start()
     {
@@ -22,13 +21,27 @@ private Rigidbody2D _rigidbody;
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
 
         if (Mathf.Approximately(0, movement))
-            transform.rotation = movement > 0 ? Quaternion.Euler(0,180,0) : Quaternion.identity;
+        transform.rotation = movement > 0 ? Quaternion.Euler(0,180,0) : Quaternion.identity;
 
         if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
         {
             _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
         }
 
-
+        //detect collision with other objects
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+    
+            if(collision.gameObject.CompareTag ("RedGround")
+                {
+                Debug.Log("RedGround");
+                }
+            if(collision.gameObject.CompareTag ("GreenGround")
+                {
+                Debug.Log("GreenGround");
+                var movement = Input.GetAxis("Horizontal");
+                transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed * SpeedMultiplier;
+                }
+        }    
     }
 }
