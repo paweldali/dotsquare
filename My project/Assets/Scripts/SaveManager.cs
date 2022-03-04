8f63c1ef70abacf5a295800fe3b1a373475b0d3f
Piotr Łach <lachpio999@gmail.com>
2022-03-04 21:59:10 +0100

    saving system wip

---
+++ My project/Assets/Scripts/SaveManager.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class SaveManager : MonoBehaviour
+{
+    public static SaveManager instance { get; private set; }
+
+    public float[] bestTimes = new float[100];
+
+    private void Awake()
+    {
+
+        if (instance != null && instance != this)
+            Destroy(gameObject);
+        else
+            instance = this;
+
+        DontDestroyOnLoad(gameObject);
+        Load();
+    }
+
+    public void Load()
+    {
+        if(File.Exists(Application.persistentDataPath + "/gameData.dat")){
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/gameData.dat", FileMode.Open);
+            GameData gameData = (GameData)bf.Deserialize(file);
+
+            bestTimes = gameData.bestTimes;
+
+            file.Close();
+        }
+    }
+
+
+    public void Save()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/gameData.dat");
+        GameData gameData = new GameData();
+
+        gameData.bestTimes = bestTimes;
+
+        bf.Serialize(file, gameData);
+        file.Close();
+    }
+}
+
+[Serializable]
+class GameData{
+    public float[] bestTimes;
+}
--- My project/Assets/Scripts/SelectLevelMenu.cs
+++ My project/Assets/Scripts/SelectLevelMenu.cs
@@ -2,15 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
 
 public class SelectLevelMenu : MonoBehaviour
 {
     // Start is called before the first frame update
 
     private GameMaster gm;
+
+    private float[] bestTimes;
+
+    
     void Start()
     {
         gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+
+
+        //best times info from SaveManager
+        bestTimes = SaveManager.instance.bestTimes;
+
+        SetBestLevelTimes();
     }
 
     // // Update is called once per frame
@@ -26,5 +38,16 @@ public class SelectLevelMenu : MonoBehaviour
 
     public void SelectLevel(){
         Debug.Log("SelectLevel");
+    }
+
+
+    //set best level times to TMPs in SelectLevelMenu
+    private void SetBestLevelTimes(){
+        // TextMeshProUGUI textField = (TextMeshProUGUI)GetComponents(typeof(Button))[1].GetComponents(typeof(TextMeshProUGUI))[1];
+        // TextMeshProUGUI textField = (TextMeshProUGUI)GetComponent("Level1Button").GetComponents(typeof(TextMeshProUGUI))[1];
+
+        // textField.text = "best time: " + bestTimes[0];
+
+        Debug.Log("best time: " + bestTimes[0]);
     }
 }
