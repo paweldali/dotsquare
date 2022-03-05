4533246908cf0cd176dcf6530a64c196574777ae
paweldali <pawel@dali.games>
2022-03-05 01:09:58 +0100

    save

--- My project/Assets/Scripts/Parallax.cs
+++ My project/Assets/Scripts/Parallax.cs
@@ -24,8 +24,8 @@ public class Parallax : MonoBehaviour
         float dist = (cam.transform.position.x * parallaxEffect);
         transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
 
-        if(temp > startpos + length) startpos += length;
-        else if (temp < startpos - length) startpos -= length;
+    //    if(temp > startpos + length) startpos += length;
+    //    else if (temp < startpos - length) startpos -= length;
     }
     
     
