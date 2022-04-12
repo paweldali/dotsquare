67d8f73d3bacdcaea2ab0deb1d185cb534442312
Piotr Wróblewski <p.wroblewski@Ocado.com>
2022-04-12 09:21:31 +0200

    CameraMovement script a head of player

--- My project/Assets/Scripts/CameraMovement.cs
+++ My project/Assets/Scripts/CameraMovement.cs
@@ -1,24 +1,44 @@
+using System;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
     public Transform target;
-    public Vector3 offset;
-    [Range(0,10)]
+    public Rigidbody2D PlayerRigidBody2D;
+    [Range(0, 1)]
     public float smoothFactor;
 
-
-
-
-    private void LateUpdate() 
+    private void LateUpdate()
     {
         follow();
     }
 
     void follow()
     {
+        Vector3 offset = cameraAHeadOfPlayer();
         Vector3 targetPosition = target.position + offset;
-        Vector3 smoothPosition = Vector3.Lerp(transform.position, targetPosition, smoothFactor*Time.fixedDeltaTime);
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, targetPosition, smoothFactor);
         transform.position = smoothPosition;
+    }
+
+    Vector3 cameraAHeadOfPlayer()
+    {
+        return new Vector3(offsetCount(PlayerRigidBody2D.velocity.x), offsetCount(PlayerRigidBody2D.velocity.y), -10);
+    }
+
+    private float offsetCount(float value)
+    {
+        if (value > 1)
+        {
+            return System.Math.Min(2f, value);
+        }
+        else if (value < -1)
+        {
+            return System.Math.Max(-2f, value);
+        }
+        else
+        {
+            return 0f;
+        }
     }
 }
