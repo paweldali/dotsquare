29bc48a07c04100b729cef7b33e1b26f5c8bcf8d
Piotr Łach <lachpio999@gmail.com>
2022-07-11 15:57:48 +0200

    redo changes of camera movement script

--- My project/Assets/Scripts/CameraMovement.cs
+++ My project/Assets/Scripts/CameraMovement.cs
@@ -15,30 +15,31 @@ public class CameraMovement : MonoBehaviour
 
     void follow()
     {
-        Vector3 offset = cameraAHeadOfPlayer();
+        // Vector3 offset = cameraAHeadOfPlayer();
+        Vector3 offset = new Vector3(0, 0, -10);
         Vector3 targetPosition = target.position + offset;
         Vector3 smoothPosition = Vector3.Lerp(transform.position, targetPosition, smoothFactor);
         transform.position = smoothPosition;
     }
 
-    Vector3 cameraAHeadOfPlayer()
-    {
-        return new Vector3(0, offsetCount(PlayerRigidBody2D.velocity.y), -10);
-    }
+    // Vector3 cameraAHeadOfPlayer()
+    // {
+    //     return new Vector3(0, offsetCount(PlayerRigidBody2D.velocity.y), -10);
+    // }
 
-    private float offsetCount(float value)
-    {
-        if (value > 0.1f)
-        {
-            return System.Math.Min(1.5f, value);
-        }
-        else if (value < -0.1f)
-        {
-            return System.Math.Max(-1.5f, value);
-        }
-        else
-        {
-            return 0f;
-        }
-    }
+    // private float offsetCount(float value)
+    // {
+    //     if (value > 0.1f)
+    //     {
+    //         return System.Math.Min(1.5f, value);
+    //     }
+    //     else if (value < -0.1f)
+    //     {
+    //         return System.Math.Max(-1.5f, value);
+    //     }
+    //     else
+    //     {
+    //         return 0f;
+    //     }
+    // }
 }
